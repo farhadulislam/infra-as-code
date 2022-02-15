d97f75bfb4941e78d5f14ffaab7379976dc2f51c
Farhd Islam <farhad.islam@outlook.com>
2022-02-15 23:59:11 +0000

    First commit - added main.tf : provider, data, and resource block

---
+++ terraform/demo/main.tf
@@ -0,0 +1,30 @@
+#################
+###### PROVIDERS
+#################
+
+provider "aws"{
+    access_key = "DUMMY"
+    secret_key = "DUMMY"
+    region = "us-east-1"
+}
+
+
+#################
+###### DATA
+#################
+
+data "aws_ssm_provider" "ami"{
+    name = "/aws/service/ami-amazon-linux-latest/amzn2-"
+}
+
+
+
+#################
+###### RESOURCES
+#################
+
+# NETWORKING #
+
+resource "aws_vpc" "vpc"{
+    cidr_block = "10.0.0.0/16"
+}
